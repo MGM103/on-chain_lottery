2c6b838d1086b57d23b7db386d3c47ce70840ef7
MGM103 <marcus.marinelli3@gmail.com>
2023-11-10 22:46:32 +1100

    Adding entry fee

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,5 +1,29 @@
-//SPDX-license-identifier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
 
-contract Lottery {}
+/**
+ * @title Lottery
+ * @author Marcus Marinelli
+ * @notice This contract is a lottery contract
+ * @dev This contract implements Chainlink VRF v2
+ */
+contract Lottery {
+    uint256 private s_entryFee;
+
+    constructor(uint256 entryFee) {
+        s_entryFee = entryFee;
+    }
+
+    function enterLottery() public payable {}
+
+    /** Setter functions */
+    function setEntryFee(uint256 newEntryFee) external {
+        s_entryFee = newEntryFee;
+    }
+
+    /** Getter functions */
+    function getEntryFee() public view returns (uint256) {
+        return s_entryFee;
+    }
+}
