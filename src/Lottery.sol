68d32c2f2f90014ea39e30754e847002a7cb5567
MGM103 <marcus.marinelli3@gmail.com>
2023-11-10 22:57:50 +1100

    Requiring ownership to update entry fee

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -10,20 +10,33 @@ pragma solidity ^0.8.19;
  */
 contract Lottery {
     uint256 private s_entryFee;
+    address private s_owner;
+
+    error Lottery__NotOwner();
 
     constructor(uint256 entryFee) {
         s_entryFee = entryFee;
+        s_owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != s_owner) revert Lottery__NotOwner();
+        _;
     }
 
     function enterLottery() public payable {}
 
     /** Setter functions */
-    function setEntryFee(uint256 newEntryFee) external {
+    function setEntryFee(uint256 newEntryFee) external onlyOwner {
         s_entryFee = newEntryFee;
     }
 
     /** Getter functions */
     function getEntryFee() public view returns (uint256) {
         return s_entryFee;
+    }
+
+    function getOwner() public view returns (address) {
+        return s_owner;
     }
 }
